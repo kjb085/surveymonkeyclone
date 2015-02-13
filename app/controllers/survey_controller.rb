af7e3a9c08ff9192dab2366f4d3529283c13928e
cpachomski <cpachomski@gmail.com>
2015-02-13 17:36:37 -0500

    finishc reating surveys and questions and additive questions

--- app/controllers/survey_controller.rb
+++ app/controllers/survey_controller.rb
@@ -3,3 +3,21 @@ get '/surveys/new' do
   erb :create_survey
 
 end
+
+post '/surveys/create' do
+  session[:id] = 1
+  @survey = Survey.create(user_id: session[:id], title: params[:title])
+
+  @question = Question.create(survey_id: @survey.id, content: params[:content])
+
+  redirect "/surveys/#{@survey.id}"
+
+end
+
+
+get '/surveys/new/add_question' do
+  puts 'CHRIS AHHHHHHHHH'
+
+  erb :_add_questions, :layout => false
+
+end
