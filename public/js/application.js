c0746f26a446ec063926107be4f5c009ec826447
cpachomski <cpachomski@gmail.com>
2015-02-13 17:36:37 -0500

    finishc reating surveys and questions and additive questions

--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,60 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+  $('.add_question').on('click',function(event){
+
+    event.preventDefault();
+
+
+
+    $.ajax(
+    {
+      type: "get",
+      url: '/surveys/new/add_question',
+      data: {qCount: $('.question').length},
+
+      success: function(response){
+
+        $('.questions-container').append(response);
+        console.log('GREAT success')
+
+      },
+
+        fail: function(){
+          console.log("YOU LOSE GOOD-DAY SIR");
+      }
+    })
+
+
+
+  })
+
+// FOR DELETING QUESTIONS WHILE MAKING THE THANGS
+
+// $('.remove-question').on('click',function(event){
+
+//     event.preventDefault();
+
+//     $.ajax(
+//     {
+//       type: "get",
+//       url: '/surveys/new/add_question',
+//       data: {qCount: $('.question').length},
+
+//       success: function(response){
+
+//         $('questions-container div:last-child',this).remove();
+//         console.log('GREAT success')
+
+//       },
+
+//         fail: function(){
+//           console.log("YOU LOSE GOOD-DAY SIR");
+//       }
+//     })
+
+
+
+//   })
+
+
 });
