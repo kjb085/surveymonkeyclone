b4d84afa2cbc04558b68f14a878571a2b7997b2b
melissawimberly <melissa.f.wimberly@gmail.com>
2015-02-13 14:20:56 -0500

    Add login routes

--- app/controllers/auth.rb
+++ app/controllers/auth.rb
@@ -1,11 +1,45 @@
 get '/' do
+  redirect "/index"
+end
+
+get '/index' do
   erb :welcome
 end
 
+post '/login' do
+  erb :login
+end
+
+# I need a seperate post for the signup page, but is that a post '/index'?
+
+
 get '/login' do
-  
+  erb :login
+end
+
+post '/login' do
+  user = User.find_by(params[:user][:email])
+
+  if user.try(:authenticate, params[:user][:password])
+    #why is this user_id? did i have to set that somewhere else?
+    session[:id] = user.id
+    redirect '/'
+  else
+    redirect '/signup'
+  end
 end
 
 get '/signup' do
-  
+  @user = User.create(params[:user])
+
+
+end
+
+post '/signup' do
+  erb :signup
+end
+
+
+get '/logout' do
+
 end
