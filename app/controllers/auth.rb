74c5b49b0ec7ba9156901dac296ecf393fd51ea2
Michael Angelo <yamikamisama@gmail.com>
2015-02-13 19:55:44 -0500

    rebase merge fix

--- app/controllers/auth.rb
+++ app/controllers/auth.rb
@@ -1,6 +1,11 @@
 get '/' do
+  redirect "/index"
+end
+
+get '/index' do
   erb :welcome
 end
+
 
 get '/surveys' do
   @surveys = Survey.all
@@ -9,6 +14,45 @@ end
 
 get '/surveys/new' do
   erb :create_survey
+end
+
+post '/login' do
+  erb :login
+end
+
+# I need a seperate post for the signup page, but is that a post '/index'?
+
+
+get '/login' do
+  erb :login
+end
+
+post '/login' do
+  user = User.find_by(params[:user][:email])
+
+  if user.try(:authenticate, params[:user][:password])
+    #why is this user_id? did i have to set that somewhere else?
+    session[:id] = user.id
+    redirect '/'
+  else
+    redirect '/signup'
+  end
+end
+
+get '/signup' do
+  @user = User.create(params[:user])
+
+
+end
+
+post '/signup' do
+  erb :signup
+end
+
+
+get '/logout' do
+
+
 end
 
 
