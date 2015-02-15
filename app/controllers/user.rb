05414d404cb53402cfdc90d9579957422250f063
Kenn Breece <kjb085@gmail.com>
2015-02-15 13:23:42 -0500

    Complete the update and elete functionality for the user resource

--- app/controllers/user.rb
+++ app/controllers/user.rb
@@ -5,24 +5,37 @@ get '/users/:id' do
   else
   	redirect '/login'
   end
+
 end
 
 
 get '/users/:id/edit' do
   @user = current_user
-  erb :user_edit
+  erb :edit_user
 end
 
-put '/users/:id' do
+put '/users/:id' do |id|
 
+  params[:edit].delete_if do |key, value|
+  	value == ""
+  end
+
+  User.find(id).update_attributes(params[:edit])
+
+  redirect "/users/#{id}"
 end
 
 
 # Change these to use AJAX instead
 get '/users/:id/delete' do
 
+  erb :delete_user
+
 end
 
-delete '/users/:id' do
+delete '/users/:id' do |id|
+
+  User.find(id).destroy
+  redirect '/login'
 
 end
