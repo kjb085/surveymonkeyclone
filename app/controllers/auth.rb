4ce6e63fa61caed69360f0a6427bae3f8bb3a4b2
melissawimberly <melissa.f.wimberly@gmail.com>
2015-02-14 19:18:00 -0500

    Complete auth routes

---
+++ app/controllers/auth.rb
@@ -0,0 +1,41 @@
+
+######################### LOGIN ###########################################
+get '/login' do
+erb :login
+end
+
+post '/login' do
+  user = User.find_by( username: params[:user][:username] )
+
+  if user.try(:authenticate, params[:user][:password])
+    session[:id] = user.id
+    redirect '/'
+  else
+    redirect '/signup'
+  end
+
+end
+
+######################### SIGNUP ###########################################
+
+get '/signup' do
+  erb :signup
+end
+
+post '/signup' do
+  user = User.create(params[:user])
+
+  if user.save
+    session[:id] = user.id
+    redirect '/'
+  else
+    redirect '/signup'
+  end
+end
+
+######################### LOGOUT ###########################################
+
+get '/logout' do
+  session[:id] = nil
+  redirect '/'
+end
