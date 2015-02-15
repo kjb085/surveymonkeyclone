ae0a62421e350edd4e3529e18c9590285c08f055
melissawimberly <melissa.f.wimberly@gmail.com>
2015-02-14 20:16:51 -0500

    Add toggle for login

--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,27 @@
 $(document).ready(function() {
 
+    $(".login_btn").on("click", function(event){
+      event.preventDefault();
+      console.log("you clicked!")
+      $(".login_form").toggle();
+    });
 
 
+    $(".login_ajax").on("submit", function(event){
+      event.preventDefault();
+      var $form = $(event.target);
+      $.ajax({
+        url: $form.attr('action'),
+        type: $form.attr('method'),
+        data: $form.serialize(),
+        success: function(response){
+          console.log("YOU ARE SUCCESFUL")
+        },
+        error: function(response){
+          console.log("YOU'VE FAILED")
+        }
+      });
+    });
 
 
   });
